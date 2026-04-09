9d83a41efa7b6f57b65b28f51276839e67df0260
Prabal Bijoy Dutta <prabaldutta.iitr@gmail.com>
2026-04-08 22:35:21 -0700

    updated iq150

---
+++ IQ150/h_index.py
@@ -0,0 +1,10 @@
+class Solution1:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        citations = citations[::-1]
+        res = 0
+        print(citations)
+        for i in range(len(citations)):
+            if citations[i]>=i+1:
+                res+=1
+        return res
