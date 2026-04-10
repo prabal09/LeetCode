1fd8b0fb281f83a7f015c47417ccc8d5288f240a
Prabal Bijoy Dutta <prabaldutta.iitr@gmail.com>
2026-04-10 00:42:09 -0700

    updated h-idx

--- IQ150/h_index.py
+++ IQ150/h_index.py
@@ -8,3 +8,20 @@ class Solution1:
             if citations[i]>=i+1:
                 res+=1
         return res
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        papers = [0]*(len(citations)+1)
+        n_papers = len(citations)
+        for i in citations:
+            if i>=len(citations):
+                papers[n_papers]+=1
+            else:
+                papers[i]+=1
+
+        h = n_papers
+        papers_c = papers[n_papers]
+        while papers_c < h:
+            h-=1
+            papers_c += papers[h]
+        return h
