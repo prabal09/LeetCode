a95f64fb4082be972a2c853de3fa004830e0bb0f
Prabal Bijoy Dutta <prabaldutta.iitr@gmail.com>
2026-05-02 03:21:19 -0700

    updated happy number solution

--- IQ150/happy_number.py
+++ IQ150/happy_number.py
@@ -18,3 +18,14 @@ class Solution:
                 n1 = summ
                 summ = 0
         return False
+
+class Solution2:
+    def isHappy(self, n: int) -> bool:
+        def get_next(number):
+            return sum(int(digit) ** 2 for digit in str(number))
+        
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+        return n == 1
