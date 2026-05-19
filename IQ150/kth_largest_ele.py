562185690d0de0787eb57acbbecbddb055b3d6c6
Prabal Bijoy Dutta <prabaldutta.iitr@gmail.com>
2026-05-19 14:22:06 -0700

    solved kth largest ele pb using max heap

---
+++ IQ150/kth_largest_ele.py
@@ -0,0 +1,15 @@
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k_lar = nums[0]
+        k_idx = k
+
+        for i in range(len(nums)):
+            nums[i] = -nums[i]
+
+        heapq.heapify(nums)
+
+        for _ in range(k-1):
+            heapq.heappop(nums)
+
+        return -heapq.heappop(nums)
