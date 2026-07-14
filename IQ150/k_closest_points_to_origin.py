7075e8373bac69739508a26f67401f716042a05c
Prabal Bijoy Dutta <prabaldutta.iitr@gmail.com>
2026-07-14 01:35:03 -0700

    updated k closest points to origin using heap

--- IQ150/k_closest_points_to_origin.py
+++ IQ150/k_closest_points_to_origin.py
@@ -3,4 +3,27 @@ class Solution:
         dist = [pt[0]**2 + pt[1]**2 for pt in points]
         points_sorted = [x for _, x in sorted(zip(dist, points))]
         return points_sorted[:k]
-        # O(nlogn)
+        # O(nlogn); O(n)
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        def dist(xi,yi):
+            return xi**2 + yi**2
+        if k == 0:
+            return []
+        if len(points) == k:
+            return points
+
+        heap = []
+        # build a heap of size k
+        for x,y in points:
+            d = dist(x,y)
+
+            if len(heap) < k:
+                heapq.heappush(heap,(-d,x,y))
+            else:
+                heapq.heappushpop(heap,(-d,x,y))
+
+        return [[x,y] for d,x,y in heap]
+
+# O(nlogk); O(k)
