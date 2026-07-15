dd8ad07334a00082f67c218ec8180c58157619e8
Prabal Bijoy Dutta <prabaldutta.iitr@gmail.com>
2026-07-15 00:06:04 -0700

    added top k frequent using bucket sort

--- IQ150/top_k_frequent.py
+++ IQ150/top_k_frequent.py
@@ -13,3 +13,25 @@ class Solution1:
                 heapq.heappushpop(heap,(count,num))
 
         return [num for _, num in heap]
+
+
+from collections import Counter
+class Solution2:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqs = Counter(nums)
+
+        bucket = [0]*(len(nums)+1)
+
+        for num,freq in freqs.items():
+            if bucket[freq] == 0:
+                bucket[freq] = [num]
+            else:
+                bucket[freq].append(num)
+
+        res = []
+        for i in range(len(nums),-1,-1):
+            if bucket[i] !=0:
+                res.extend(bucket[i])
+            if len(res) == k:
+                break
+        return res
